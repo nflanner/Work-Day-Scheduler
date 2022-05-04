a654ad999fc815a0c448c901fe757bb783681dd1
Nolan Flannery <nolanflannery@gmail.com>
2022-05-04 10:46:18 -0700

    styled the submit buttons, coloring working properly

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -1,15 +1,46 @@
 var headerEl = $("#currentDay");
-var saveBtnEl = $('.saveBtn');
+var saveBtnEl = $('.btn');
+
+var events = {
+    0:"",
+    1:"",
+    2:"",
+    3:"",
+    4:"",
+    5:"",
+    6:"",
+    7:"",
+    8:""
+};
 
 getDay();
+getColors();
 
 function getDay() {
     var currentDay = moment().format("dddd, MMMM Do");
     headerEl.text(currentDay);
 }
 
+function getColors() {
+    var currentHour = parseInt(moment().format("HH"));
+    console.log(currentHour);
+    var allRows = $(".row");
+    for (var i = 0; i < allRows.length; i++) {
+        var rowHour = i + 9
+        if (rowHour < currentHour) {
+            // allRows[i].querySelector('.col-10').className = 'col-10 past';
+            allRows[i].className = 'row past';
+        } else if (rowHour ==  currentHour) {
+            allRows[i].className = 'row present';
+        } else {
+            allRows[i].className = 'row future';
+        }
+    }
+}
+
 saveBtnEl.on('click', function (event) {
     // save the input text
     var index = event.currentTarget.getAttribute('data-index');
+    console.log(index);
   });
   
