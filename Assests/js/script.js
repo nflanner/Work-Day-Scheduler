53b9ea09c723af079f045a68fed60f39a363b98c
Nolan Flannery <nolanflannery@gmail.com>
2022-05-04 11:53:30 -0700

    finsished!

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -1,7 +1,8 @@
 var headerEl = $("#currentDay");
 var saveBtnEl = $('.btn');
+var contentEls = document.getElementsByTagName('input');
 
-var events = {
+var eventsLocal = {
     0:"",
     1:"",
     2:"",
@@ -13,17 +14,20 @@ var events = {
     8:""
 };
 
+// upon loading, set the date, row colors based off local time, and load locally-saved content
 getDay();
 getColors();
+loadEventContent();
 
+// this formats the date in the header
 function getDay() {
     var currentDay = moment().format("dddd, MMMM Do");
     headerEl.text(currentDay);
 }
 
+// this sets all the row colors
 function getColors() {
     var currentHour = parseInt(moment().format("HH"));
-    console.log(currentHour);
     var allRows = $(".row");
     for (var i = 0; i < allRows.length; i++) {
         var rowHour = i + 9
@@ -38,9 +42,33 @@ function getColors() {
     }
 }
 
+// this saves all event content to local storage
+function saveEventContent() {
+    localStorage.setItem("eventContent", JSON.stringify(eventsLocal));
+}
+
+// this loads in previously-saved event content from local storage
+function loadEventContent() {
+    var prevContent = JSON.parse(localStorage.getItem("eventContent"));
+    var contentList = contentEls;
+    for (var i = 0; i < Object.keys(eventsLocal).length; i++) {
+        if (prevContent[i] != ""){
+            contentList[i].value = prevContent[i];
+            eventsLocal[i] = prevContent[i];
+        } else {
+            contentList[i].value = "";
+            eventsLocal[i] = "";
+        }
+    }
+}
+
+// this is our event listener
 saveBtnEl.on('click', function (event) {
+    event.preventDefault();
     // save the input text
     var index = event.currentTarget.getAttribute('data-index');
-    console.log(index);
+    var eventContent = contentEls[index].value;
+    eventsLocal[index] = eventContent;
+    saveEventContent();
   });
   
